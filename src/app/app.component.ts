ead981662b7067a0310980c31c95ba77c7eb3f9d
chaitanya150895 <chaitanya150895@gmail.com>
2019-11-07 17:54:46 +0530

    google/facebook login provided

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService, SocialUser, GoogleLoginProvider, FacebookLoginProvider } from 'ng4-social-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'social-login';
+  title: 'App';
+  public user: any = SocialUser;
+
+  constructor(private socialAuthService: AuthService){}
+ facebookLogin(){
+   this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then((userData) => {
+   this.user = userData;
+  });
+ }
+
+ googleLogin(){
+   this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then((userData) => {
+     this.user = userData;
+   });
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider } from 'ng4-social-login';
+import {HttpClientModule} from '@angular/common/http'
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider('566700667916-od90oj8l2p4asgcqdfaq14p4hcmgpr2v.apps.googleusercontent.com')
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('3108862749187143')
+  }
+
+], false);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +27,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: AuthServiceConfig, useFactory: provideConfig}],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
